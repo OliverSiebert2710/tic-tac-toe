75496f8205f7adaff121a381d1a374221a4e11be
OliverSiebert2710 <s.oliver.2710@gmail.com>
2023-11-21 12:06:51 +0100

    onclick function added

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 let fields = [
     null,
-    'circle',
     null,
     null,
     null,
     null,
     null,
     null,
-    'cross',
+    null,
+    null,
 ];
+
+let currentPlayer = 'circle';
 
 function init() {
     render();
 }
 
 function render() {
-    const contentDiv = document.getElementById('content');
+  const contentDiv = document.getElementById('content');
 
-    // Generate table HTML
-    let tableHtml = '<table>';
-    for (let i = 0; i < 3; i++) {
-        tableHtml += '<tr>';
-        for (let j = 0; j < 3; j++) {
-            const index = i * 3 + j;
-            let symbol = '';
-            if (fields[index] === 'circle') {
-                symbol = generateCircleSVG();
-            } else if (fields[index] === 'cross') {
-                symbol = generateCrossSVG();
-            }
-            tableHtml += `<td>${symbol}</td>`;
-        }
-        tableHtml += '</tr>';
-    }
-    tableHtml += '</table>';
+  // Generate table HTML
+  let tableHtml = '<table>';
+  for (let i = 0; i < 3; i++) {
+      tableHtml += '<tr>';
+      for (let j = 0; j < 3; j++) {
+          const index = i * 3 + j;
+          tableHtml += `<td onclick="handleCellClick(${index})">${fields[index] ? generateSymbolHTML(fields[index]) : ''}</td>`;
+      }
+      tableHtml += '</tr>';
+  }
+  tableHtml += '</table>';
 
-    // Set table HTML to contentDiv
-    contentDiv.innerHTML = tableHtml;
+  // Set table HTML to contentDiv
+  contentDiv.innerHTML = tableHtml;
+}
+
+function generateSymbolHTML(symbol) {
+  return (symbol === 'circle') ? generateCircleSVG() : generateCrossSVG();
+}
+
+function handleCellClick(index) {
+  if (!fields[index]) {
+      fields[index] = currentPlayer;
+      const cell = document.getElementsByTagName('td')[index];
+      cell.innerHTML = generateSymbolHTML(currentPlayer);
+      cell.onclick = null; // Remove onclick function
+      currentPlayer = (currentPlayer === 'circle') ? 'cross' : 'circle';
+  }
 }
 
 
